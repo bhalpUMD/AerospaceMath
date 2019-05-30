05cfb793291075ec21d4c2d402fc9bbc55061146
b-halp <blake.halp@gmail.com>
2019-05-30 08:37:36 -0400

    Added matrix math outline

---
+++ AerospaceMath/Math/MatrixMath.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AerospaceMath.Math
+{
+    /// <summary>
+    /// Matrix helper functions.
+    /// </summary>
+    public class MatrixMath
+    {
+
+    }
+}
